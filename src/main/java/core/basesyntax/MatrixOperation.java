7505b156d155f934d18d531c13ad153a8db32d13
Tsivka Galina <galinatsivka@gmail.come>
2020-07-13 00:33:13 +0300

    change methods

--- src/main/java/core/basesyntax/MatrixOperation.java
+++ src/main/java/core/basesyntax/MatrixOperation.java
@@ -2,34 +2,27 @@ package core.basesyntax;
 
 public class MatrixOperation {
     public int[] getDiagonal(int[][] matrix) {
-        int[] mainDiagonal = new int[matrix.length];
-        int g = 0;
+        // int[] mainDiagonal = new int[matrix.length];
+        int[] diagonal = new int[matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                if (i == j) {
-                    mainDiagonal[g] = matrix[i][j];
-                    g++;
-                }
-            }
+            diagonal[i] = matrix[i][i];
         }
-        return mainDiagonal;
+        return diagonal;
     }
 
     public int[] getCounterDiagonal(int[][] matrix) {
-        int[] mainDiagonal = new int[matrix.length];
+        int[] diagonal = new int[matrix.length];
         for (int i = matrix.length - 1; i >= 0; i--) {
-            mainDiagonal[matrix.length - 1 - i] = matrix[matrix.length - 1 - i][i];
+            diagonal[matrix.length - 1 - i] = matrix[matrix.length - 1 - i][i];
         }
-        return mainDiagonal;
+        return diagonal;
     }
 
     public int getMin(int[][] matrix) {
-        int min = 0;
+        int min = matrix[0][0];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                if (i == 0 && j == 0) {
-                    min = matrix[i][j];
-                } else if (matrix[i][j] < min) {
+                if (matrix[i][j] < min) {
                     min = matrix[i][j];
                 }
             }
@@ -38,15 +31,15 @@ public class MatrixOperation {
     }
 
     public int getMax(int[][] matrix) {
-        int max = 0;
+        int max = matrix[0][0];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                if (i == 0 && j == 0) {
+                if (matrix[i][j] > max) {
                     max = matrix[i][j];
-                } else if (matrix[i][j] > max) {
-                    max = matrix[i][j];
+
                 }
             }
+
         }
         return max;
     }
